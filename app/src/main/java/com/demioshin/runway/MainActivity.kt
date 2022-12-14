939dac5e76ee1528bdd55989d7035b348777bda9
Demi Oshin <demiaoshin@gmail.com>
2022-12-14 17:05:01 +0000

    Sort out permissions within app

--- app/src/main/java/com/demioshin/runway/MainActivity.kt
+++ app/src/main/java/com/demioshin/runway/MainActivity.kt
@@ -1,21 +1,24 @@
 package com.demioshin.runway
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,6 +30,8 @@ import com.demioshin.runway.ui.theme.RunwayTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.accompanist.permissions.rememberPermissionState
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -41,16 +46,97 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colors.background
             ) {
-                BetterMap(
-                    viewModel = viewModel
-                )
+                Test(viewModel = viewModel)
             }
         }
     }
 }
 
+
+@ExperimentalPermissionsApi
+@Composable
+fun RequireLocationPermission(
+    navigateToSettingsScreen: () -> Unit,
+    content: @Composable() () -> Unit
+) {
+    // Track if the user doesn't want to see the rationale any more.
+    var doNotShowRationale by rememberSaveable { mutableStateOf(false) }
+
+    // Permission state
+    val permissionState = rememberMultiplePermissionsState(
+        listOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    )
+
+    when {
+        permissionState.allPermissionsGranted -> {
+            content()
+        }
+        // If the user denied the permission but a rationale should be shown, or the user sees
+        // the permission for the first time, explain why the feature is needed by the app and allow
+        // the user to be presented with the permission again or to not see the rationale any more.
+        permissionState.shouldShowRationale ||
+                !permissionState.permissionRequested -> {
+            if (doNotShowRationale) {
+                Text("Feature not available")
+            } else {
+                Column {
+                    Text("Need to detect current location. Please grant the permission.")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Row {
+                        Button(onClick = { permissionState.launchMultiplePermissionRequest() }) {
+                            Text("Request permission")
+                        }
+                        Spacer(Modifier.width(8.dp))
+                        Button(onClick = { doNotShowRationale = true }) {
+                            Text("Don't show rationale again")
+                        }
+                    }
+                }
+            }
+        }
+        // If the criteria above hasn't been met, the user denied the permission. Let's present
+        // the user with a FAQ in case they want to know more and send them to the Settings screen
+        // to enable it the future there if they want to.
+        else -> {
+            Column {
+                Text(
+                    "Request location permission denied. " +
+                            "Need current location to show nearby places. " +
+                            "Please grant access on the Settings screen."
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Button(onClick = navigateToSettingsScreen) {
+                    Text("Open Settings")
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun Test(viewModel: MapViewModel) {
+    val context = LocalContext.current
+
+    RequireLocationPermission(navigateToSettingsScreen = {
+        context.startActivity(
+            Intent(
+                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.fromParts("package", context.packageName, null)
+            )
+        )
+    }) {
+        BetterMap(viewModel = viewModel)
+    }
+}
+
 @Composable
 fun BetterMap(viewModel: MapViewModel) {
+    val context = LocalContext.current
+
     var uiSettings by remember { mutableStateOf(MapUiSettings()) }
     val properties by remember {
         mutableStateOf(MapProperties(mapType = MapType.NORMAL))
