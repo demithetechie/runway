8e9890ef42acb0e4e3b8f7622fc81130db54ae0a
Demi Oshin <demiaoshin@gmail.com>
2022-12-29 19:54:52 +0000

    add step counter

---
+++ app/src/main/java/com/demioshin/runway/services/StepsManager.kt
@@ -0,0 +1,35 @@
+package com.demioshin.runway.services
+
+import android.content.Context
+import android.content.Context.SENSOR_SERVICE
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.lifecycle.MutableLiveData
+
+class StepsManager(context: Context) : SensorEventListener {
+
+    val steps = MutableLiveData<Int>()
+    private var sensorManager: SensorManager? = null
+    private var stepSensor: Sensor? = null
+
+//    private var initialSteps = -1
+
+    init {
+        sensorManager = context.getSystemService(SENSOR_SERVICE) as SensorManager
+        stepSensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        sensorManager!!.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_FASTEST)
+    }
+
+    override fun onSensorChanged(sensorEvent: SensorEvent?) {
+        val newSteps = sensorEvent!!.values.firstOrNull()
+
+        if (newSteps != null) {
+            steps.value = newSteps.toInt()
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) = Unit
+}
